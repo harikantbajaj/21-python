e351c0b72930dc33a4e0f9052978f4e49d4b6d5a
Harikant Bajaj <harikantb08@gmail.com>
2025-04-19 23:00:48 +0530

    d

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,45 +1,39 @@
-import random   
+import random
 
-user_win =0
-computer_wins=0
+user_wins = 0
+computer_wins = 0
 
-options=["rock","paper","scissors","test"]
+options = ["rock", "paper", "scissors"]
 
 while True:
-    user_input= input("Type Rock/paper/Scissors or q to Quit  ").lower()
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
         break
-    
-    if user_input not in["rock","paper","scissors"]:
+
+    if user_input not in options:
         continue
-    random_number = random.randint(0,2)
-    
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
     computer_pick = options[random_number]
-    print("computer picked", computer_pick+".")
-    
-    if user_input =="rock" and computer_pick =="scissors":
-        print("you win")
-        user_win +=1
-        
-    elif user_input =="paper" and computer_pick =="rock":
-        print("you win")
-        user_win +=1
-        
-    elif user_input =="scissors" and computer_pick =="paper":
-        print("you win")
-        user_win +=1
-    elif user_input =="rock" and computer_pick =="rock":
-        print("same choosen")
-        continue
-        
-    elif user_input =="paper" and computer_pick =="paper":
-        print("same choosen")
+    print("Computer picked", computer_pick + ".")
 
-        continue        
-    elif user_input =="scissors" and computer_pick =="scissors":
-        print("same choosen")
-        continue
-print("you won"+user_win+"times")
-print("computer won"+computer_wins+"times")
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
 
-print("Goodbye!!")
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
---
+++ choose_your_own_adventure.py
@@ -0,0 +1,17 @@
+name= input("type your name")
+print("welcome" ,name, "to this adventure ")
+
+answer=input("you are in a dirt road, it has comr to an end and you can go left or right. which way would you like to go.").lower()
+
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+    if answer=="swin":
+        print("you swam accross and you are now on the other side of the river")
+    elif answer=="walk":
+        print*
+
+
+if  answer == "right":
+    
