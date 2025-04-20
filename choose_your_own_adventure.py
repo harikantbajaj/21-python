6ae0412ed362970c2a6f3a09329c57d14e6eee9d
Harikant Bajaj <harikantb08@gmail.com>
2025-04-20 17:10:01 +0530

    DONE

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,17 +1,29 @@
-name= input("type your name")
+name= input("Type Your Name ")
 print("welcome" ,name, "to this adventure ")
 
-answer=input("you are in a dirt road, it has comr to an end and you can go left or right. which way would you like to go.").lower()
+answer=input("you are in a dirt road, it has come to an end and you can go left or right. which way would you like to go. ").lower()
 
 
 if answer == "left":
     answer = input(
         "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
-    if answer=="swin":
+    if answer=="swim":
         print("you swam accross and you are now on the other side of the river")
     elif answer=="walk":
-        print*
+        print("you walked around the river and you are now on the other side of the river")
+    else:
+        print("not a valid option")
 
 
-if  answer == "right":
-    
+elif  answer == "right":
+    answer=input("You are on the bridge and have 2 options choose a jump in the river or eaten up by zombies- type zombie or jump ")
+    if answer == "zombie":
+        print("you are now a zombie and you are eating brains")
+    elif answer=="jump":
+        print("you jumped in the river and you are now on the other side of the river")
+    else:
+        print("not a valid option")
+        
+                       
+else:
+    print("not a valid option")
